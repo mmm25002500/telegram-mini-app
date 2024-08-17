cca9488583f8eb5b0db18de40dff1401e9d44b1f
mmm25002500 <marryhan25648@gmail.com>
2024-08-18 01:53:55 +0800

    fix: telegram mini app detect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,49 +2,49 @@ import { useEffect, useState } from 'react';
 import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // const [isInTelegram, setIsInTelegram] = useState(false);
+  const [isInTelegram, setIsInTelegram] = useState(false);
 
-  // // 检测是否在 Telegram Web App 中
-  // const isInTelegramWebApp = () => {
-  //   return (
-  //     typeof window !== 'undefined' &&
-  //     typeof window.Telegram !== 'undefined' &&
-  //     typeof window.Telegram.WebApp !== 'undefined'
-  //   );
-  // };
+  // 检测是否在 Telegram Web App 中
+  const isInTelegramWebApp = () => {
+    return (
+      typeof window !== 'undefined' &&
+      typeof window.Telegram !== 'undefined' &&
+      typeof window.Telegram.WebApp !== 'undefined'
+    );
+  };
 
-  // useEffect(() => {
-  //   const telegramDetected = isInTelegramWebApp();
-  //   setIsInTelegram(telegramDetected);
+  useEffect(() => {
+    const telegramDetected = isInTelegramWebApp();
+    setIsInTelegram(telegramDetected);
 
-  //   if (telegramDetected) {
-  //     const tg = window.Telegram.WebApp;
-  //     tg.ready();
-  //     console.log('Telegram Web App is ready.');
+    if (telegramDetected) {
+      const tg = window.Telegram?.WebApp;
+      tg?.ready();
+      console.log('Telegram Web App is ready.');
 
-  //     // 设置主按钮
-  //     tg.MainButton.setText('Click me');
-  //     tg.MainButton.show();
+      // 设置主按钮
+      tg?.MainButton.setText('Click me');
+      tg?.MainButton.show();
 
-  //     tg.MainButton.onClick(() => {
-  //       tg.sendData('Button clicked!');
-  //       console.log('Main button clicked.');
-  //     });
-  //   } else {
-  //     console.log('Not running inside Telegram Web App.');
-  //   }
-  // }, []);
+      tg?.MainButton.onClick(() => {
+        tg?.sendData('Button clicked!');
+        console.log('Main button clicked.');
+      });
+    } else {
+      console.log('Not running inside Telegram Web App.');
+    }
+  }, []);
 
   return (
     <div>
-      {/* {isInTelegram ? ( */}
+      {isInTelegram ? (
         <Component {...pageProps} />
-      {/* ) : (
+      ) : (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
           <h1>请使用 <strong>Telegram</strong> 打开</h1>
           <p>此应用程序只能在 Telegram Web App 中使用。</p>
         </div>
-      )} */}
+      )}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,16 @@
 import { useEffect, useState } from 'react';
 
 const IndexPage = () => {
-  const [isTelegramWebApp, setIsTelegramWebApp] = useState(false);
-
-  useEffect(() => {
-    console.log('Running useEffect...');
-
-    if (window.Telegram) {
-      console.log('Telegram object detected:', window.Telegram);
-      
-      if (window.Telegram.WebApp) {
-        console.log('Telegram Web App object detected:', window.Telegram.WebApp);
-        
-        const tg = window.Telegram.WebApp;
-        tg.ready();
-        setIsTelegramWebApp(true);
-        console.log('Telegram Web App is ready.');
-
-        tg.MainButton.setText('Click me');
-        tg.MainButton.show();
-
-        tg.MainButton.onClick(() => {
-          tg.sendData('Button clicked!');
-          console.log('Main button clicked.');
-        });
-      } else {
-        console.log('Telegram Web App is not ready.');
-      }
-    } else {
-      console.log('Telegram Web App not detected.');
-    }
-  }, []);
 
   return (
-    <div>
-      {isTelegramWebApp ? (
-        <p>Telegram Web App detected.</p>
-      ) : (
-        <p>Telegram Web App not detected.</p>
-      )}
-    </div>
+    // <div>
+    //   {isTelegramWebApp ? (
+    //     <p>Telegram Web App detected.</p>
+    //   ) : (
+    //     <p>Telegram Web App not detected.</p>
+    //   )}
+    // </div>
+    <p>平台：{window.Telegram?.WebApp.platform}</p>
   );
 };
 
